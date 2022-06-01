9660c69a4bfcb5590b5d3896c6fbf76bd7fdbf34
Daniel Mason <danmasson0@gmail.com>
2022-06-01 16:55:25 -0400

    implemented AboutUs section with responsive css styles, and filled out AboutUs functional component with JSX

--- src/containers/AboutUs/AboutUs.jsx
+++ src/containers/AboutUs/AboutUs.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import "./AboutUs.css";
+import images from '../../constants/images';
 
 const AboutUs = () => {
     return (
-        <div>
-
-        </div>
+        <main className="app__aboutus app__bg flex__center section__padding" id='about'>
+            <div className="app__aboutus-overlay flex__center">
+                <img src={images.G} alt="G_overlay" />
+            </div>
+            <div className="app__aboutus-content flex__center">
+                <div className="app__aboutus-content_about">
+                    <h1 className="headtext__cormorant">
+                        About Us
+                    </h1>
+                    <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+                    <p className="p__opensans">Boo</p>
+                    <button type="button" className="custom__button">No More</button>
+                </div>
+                <div className="app__aboutus-content_knife flex__center">
+                    <img src={images.knife} alt="knife" />
+                </div>
+                <div className="app__aboutus-content_history">
+                    <h1 className="headtext__cormorant">
+                        Our History
+                    </h1>
+                    <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+                    <p className="p__opensans">Boo</p>
+                    <button type="button" className="custom__button">No More</button>
+                </div>
+            </div>
+        </main >
     )
 }
 
