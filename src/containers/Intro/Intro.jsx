13c3d143bf9bf301bb41087c9792bea4a6984eb4
Daniel Mason <danmasson0@gmail.com>
2022-06-05 10:56:54 -0400

    implemented video section, with an embedded video and pause/play overlay button.

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./Intro.css";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
 
 const Intro = () => {
-    return (
-        <div>
+    const [playVideo, setPlayVideo] = useState(false);
+    const vidRef = useRef();
 
+    return (
+        <div className='app__video'>
+            <video src={meal} type="video/mp4" loop controls={false} muted ref={vidRef} />
+            <div className="app__video-overlay flex__center">
+                <div
+                    onClick={() => {
+                        setPlayVideo(!playVideo);
+                        if (playVideo) {
+                            vidRef.current.pause();
+                        } else {
+                            vidRef.current.play();
+                        };
+                    }}
+                    className='app__video-overlay_circle flex__center'>
+                    {playVideo ? (<BsPauseFill color="#fff" fontSize={30} />) : <BsFillPlayFill color="#fff" fontSize={30} />}</div>
+                <div />
+            </div>
         </div>
     )
 }
