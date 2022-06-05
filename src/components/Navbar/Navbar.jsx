f824c8c586386374a1431a96604c20d453bb18dd
Daniel Mason <danmasson0@gmail.com>
2022-06-05 11:40:39 -0400

    implemented React 18 upgrades to root index.js, changed, class, to, className in Navbar.js, and finished implementing Laurels components with respective stlyes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <li className='p__opensans'><a href="#awards">Awards</a></li>
                 <li className='p__opensans'><a href="#contact">Contact</a></li>
             </ul>
-            <div class="app__navbar-login">
+            <div className="app__navbar-login">
                 <a href="#login" className="p__opensans">Login / Register</a>
                 <div />
                 <a href="/" className="p__opensans">Book Table</a>
